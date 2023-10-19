6c36daf9b189b5ed593b8f0311c1981436557b27
vladislave11 <jordan.rome@gmail.com>
2023-10-19 20:22:31 +0300

    flask lessons

--- pizza.py
+++ pizza.py
@@ -1,6 +1,23 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__, template_folder="templates", static_folder="static")
+
+menus = [
+    {"dishes": "Poultry", "price": 46},
+    {"dishes": "Salami", "price": 51},
+    {"dishes": "Sausage", "price": 92},
+    {"dishes": "Beef", "price": 19},
+    {"dishes": "Pepperoni", "price": 7},
+]
+@app.route("/")
+@app.route("/index/")
+def picture():
+    context = {
+        'pizzas': menus
+    }
+    return render_template("pizza.html", **context)
+
+
 
 
 
